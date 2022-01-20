09b0454c47b5e78e5c449e65dedbec0216184134
arcjjang <arcelias@naver.com>
2022-01-20 11:26:47 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용 - 관심사의 분리

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,12 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
@@ -16,4 +16,5 @@ public class MemberApp {
         System.out.println("new member = " + member.getName());
         System.out.println("find member = " + findMember.getName());
     }
+
 }
